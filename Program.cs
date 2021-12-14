dfa5ca6e8ba73fe4a4e7ea512d840fccdb5a1eca
carolineelise <90719451+carolineelise@users.noreply.github.com>
2021-12-14 14:56:52 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PigDie
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var pd = new pigdie();
+            var newhighScore = pd.RunToHighScore(1000);
+            var highScore = 0;
+            for (var idx = 0; idx < 100; idx++)
+            {
+              var score = pd.PlayPigDice();
+                if (score > highScore)
+                {
+                  highScore = score;
+                    Console.WriteLine($"New high score is {newhighScore}");
+                }
+            }
+        }
+    }
+}
---
+++ pigdie.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PigDie
+{
+    public class pigdie
+    {
+        private Random rnd = new Random();
+        public int RunToHighScore(int highScore) { 
+            Console.WriteLine( $"Begin : {DateTime.Now}");
+            var score = PlayPigDice();
+            var currHighScore = 0;
+            while (score < highScore){
+                score = PlayPigDice();
+                if (score > currHighScore) { currHighScore = score; Console.WriteLine($"New high score is {score}"); }
+            }
+            return score;
+        }
+
+        public int PlayPigDice()
+        {
+            var score = 0;
+            var roll = RollDice();
+            while (roll != 1) { score += roll; roll = RollDice(); }
+            return score;
+        }
+        private int RollDice() {
+            return rnd.Next(1, 7);
+        }
+    }
+}
